0474f5051a9c32d18b77637963a68a5229f3fa24
Ricurrency <mainemail.riclub@gmail.com>
2023-02-28 19:59:37 +0800

    Removed unnecessary files
    In this update, we removed some files that the super dumb Wei Yuan Ng uploaded.

--- replit.nix
+++
@@ -1,9 +0,0 @@
-{ pkgs }: {
-	deps = [
-    pkgs.python310
-    pkgs.nodejs-16_x
-    pkgs.nodePackages.vscode-langservers-extracted
-    pkgs.nodePackages.typescript-language-server
-    pkgs.nodePackages.yarn
-	];
-}
